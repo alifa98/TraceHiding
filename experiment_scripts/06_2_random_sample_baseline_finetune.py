7722c9053ae692a26f890d5b3bb27765d60e8720
Ali Faraji <billa.svpc@gmail.com>
2024-07-10 14:15:57 -0400

    refactor: Update experiment script for random sample baseline finetune to include wandb logging

--- experiment_scripts/06_2_random_sample_baseline_finetune.py
+++ experiment_scripts/06_2_random_sample_baseline_finetune.py
@@ -1,5 +1,7 @@
 import os
 import sys
+
+import wandb
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 
 from utility.functions import custom_collate_fn
@@ -12,7 +14,7 @@ import time
 import json
 import tqdm
 
-os.environ['CUDA_VISIBLE_DEVICES'] = '5'
+os.environ['CUDA_VISIBLE_DEVICES'] = '0'
 torch.set_float32_matmul_precision('high')
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(message)s')
 
@@ -21,20 +23,37 @@ logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(message)s')
 DATASET_NAME = "nyc_checkins"
 MODEL_NAME = "LSTM"
 
-FINETUNING_BATCH_SIZE = 10
+RANDOM_SAMPLE_UNLEARNING_SIZES = [10, 20, 50, 100, 200, 300, 600, 1000]
+FINETUNING_BATCH_SIZES = [5, 10, 20, 30, 40, 40, 50, 100]
 PORTION_OF_FINE_TUNING_DATA = 0.5
 FINE_TUNING_EPOCHS = 15
 FINE_TUNING_LEARNING_RATE = 5*1e-5
 
-
 # ------------------------------------- END CONFIGURATIONS -------------------------------------#
-RANDOM_SAMPLE_UNLEARNING_SIZES = [10, 20, 50, 100, 200, 300, 600, 1000]
 REPETITIONS_OF_EACH_SAMPLE_SIZE = 5
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-for sample_size in RANDOM_SAMPLE_UNLEARNING_SIZES:
+for sample_size, batch_size in zip(RANDOM_SAMPLE_UNLEARNING_SIZES, FINETUNING_BATCH_SIZES):
     for i in range(REPETITIONS_OF_EACH_SAMPLE_SIZE):
+
+        ## create a new wandb run
+        wandb.init(
+            project="thesis_unlearning",
+            job_type="baseline",
+            name=f"finetune-{DATASET_NAME}-{MODEL_NAME}-sample_size_{sample_size}-repetition_{i}",
+            config={
+                "method_name": "finetune",
+                "dataset": DATASET_NAME,
+                "model": MODEL_NAME,
+                "sample_size": sample_size,
+                "batch_size": batch_size,
+                "repetition": i,
+                "learning_rate": FINE_TUNING_LEARNING_RATE,
+                "portion_of_fine_tuning_data": PORTION_OF_FINE_TUNING_DATA,
+                "fine_tuning_epochs": FINE_TUNING_EPOCHS
+            }
+        )
 
         # results folder
         os.makedirs(f"experiments/{DATASET_NAME}/unlearning/sample_size_{sample_size}/sample_{i}/{MODEL_NAME}/finetune/", exist_ok=True)
@@ -52,7 +71,7 @@ for sample_size in RANDOM_SAMPLE_UNLEARNING_SIZES:
         # select the random portion of the remaining data for fine-tuning
         remaining_dataset = Subset(remaining_dataset, torch.randperm(len(remaining_dataset))[:int(PORTION_OF_FINE_TUNING_DATA*len(remaining_dataset))])
         
-        remaining_dloader = DataLoader(remaining_dataset, batch_size=FINETUNING_BATCH_SIZE, collate_fn=custom_collate_fn, shuffle=True, num_workers=24)
+        remaining_dloader = DataLoader(remaining_dataset, batch_size=batch_size, collate_fn=custom_collate_fn, shuffle=True, num_workers=24)
         unlearning_dloader = DataLoader(unlearning_dataset, batch_size=len(unlearning_dataset), collate_fn=custom_collate_fn, num_workers=24)
         test_dloader = DataLoader(test_data, batch_size=len(test_data), collate_fn=custom_collate_fn, num_workers=24)
 
@@ -65,6 +84,7 @@ for sample_size in RANDOM_SAMPLE_UNLEARNING_SIZES:
         for unlearning_epoch in range(FINE_TUNING_EPOCHS):
             epoch_stats = {}
             start_epoch_time = time.time()
+            total_epoch_loss = 0
             pbar = tqdm.tqdm(remaining_dloader, desc=f"Unlearning Epoch: {unlearning_epoch}")
             for remaining_batch in pbar:
                 model.train()
@@ -86,6 +106,8 @@ for sample_size in RANDOM_SAMPLE_UNLEARNING_SIZES:
                 loss.backward()
                 optimizer.step()
 
+                total_epoch_loss += loss.item()
+                
             end_epoch_time = time.time()
 
             model.eval()
@@ -110,9 +132,15 @@ for sample_size in RANDOM_SAMPLE_UNLEARNING_SIZES:
             
             epoch_stats["unlearning_epoch_time"] = end_epoch_time - start_epoch_time
             
+            #wand logging
+            wandb.log(epoch_stats | {"unlearning_loss": total_epoch_loss})
+            
             # save unlearning model for this epoch
             torch.save(model, f"experiments/{DATASET_NAME}/unlearning/sample_size_{sample_size}/sample_{i}/{MODEL_NAME}/finetune/unlearned_epoch{unlearning_epoch}_{MODEL_NAME}_model.pt")
             
             unlearning_stats[unlearning_epoch] = epoch_stats
-        json.dump(unlearning_stats, open(f"experiments/{DATASET_NAME}/unlearning/sample_size_{sample_size}/sample_{i}/{MODEL_NAME}/finetune/unlearning_stats-batch_size_{FINETUNING_BATCH_SIZE}.json", "w"))
+            
+        wandb.finish()
+        
+        json.dump(unlearning_stats, open(f"experiments/{DATASET_NAME}/unlearning/sample_size_{sample_size}/sample_{i}/{MODEL_NAME}/finetune/unlearning_stats-batch_size_{batch_size}.json", "w"))
         logging.info(f"Unlearning models for each epoch now are saved for sample size {sample_size}, no. {i}")
