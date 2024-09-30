ec8eddb1dcf2972edd6f375b89211689c735b783
Ali Faraji <billa.svpc@gmail.com>
2024-09-30 00:00:03 -0400

    Refactor CoverageDiversityImportance.py to calculate sequence diversity

--- utility/CoverageDiversityImportance.py
+++ utility/CoverageDiversityImportance.py
@@ -1,3 +1,4 @@
+import math
 from tqdm import tqdm
 from utility.ImportanceCalculator import ImportanceCalculator
 
@@ -6,8 +7,20 @@ class CoverageDiversityImportance(ImportanceCalculator):
     def prepare(self, dataset):
         self.dataset_size = len(dataset)
         self.total_unique_hexagons = set()
+        
+        self.sequnce_diversity = []
+        
         for sequence, uesr_id in tqdm(dataset, desc="Calculating dataset unique hexagons"):
-            self.total_unique_hexagons.update(sequence)           
+            self.total_unique_hexagons.update(sequence)
+            
+        for sequence, uesr_id in tqdm(dataset, desc="Calculating dataset sequence diversity"):
+            self.sequnce_diversity.append(len(set(sequence))/len(self.total_unique_hexagons))
+            
+        self.mean_diversity = sum(self.sequnce_diversity) / len(self.sequnce_diversity)
+        self.std_diversity = math.sqrt(sum([(diversity - self.mean_diversity) ** 2 for diversity in self.sequnce_diversity]) / len(self.sequnce_diversity))
+        self.max_diversity = max(self.sequnce_diversity)
+        self.min_diversity = min(self.sequnce_diversity)
+        
 
     def calculate_importance(self, batch):
         sequences, user_ids = batch
@@ -19,5 +32,11 @@ class CoverageDiversityImportance(ImportanceCalculator):
             # Calculate the number of unique hexagons in the sequence
             unique_hexagons_count = len(set(seq.tolist()))
             batch_importance.append(unique_hexagons_count / len(self.total_unique_hexagons))
+            
+        # Min-max normalization
+        batch_importance = [
+            (importance - self.min_diversity) / (self.max_diversity - self.min_diversity)
+            for importance in batch_importance
+        ]
 
-        return [i/max(batch_importance) for i in batch_importance]
+        return batch_importance
