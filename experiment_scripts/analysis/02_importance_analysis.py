11d622ad249f4d99772d92a9cd10db9ec4d1d009
Ali Faraji <billa.svpc@gmail.com>
2024-10-11 20:46:50 -0400

    frequency of visit importance calculation

--- experiment_scripts/analysis/02_importance_analysis.py
+++ experiment_scripts/analysis/02_importance_analysis.py
@@ -4,11 +4,10 @@ import sys
 import numpy as np
 sys.path.append(os.path.join(os.path.dirname(__file__), '../..'))
 
+from utility.FrequencyOfVisitImportance import FrequencyImportance
 from utility.functions import custom_collate_fn
-from utility.ImportanceCalculator import ImportanceCalculator
 from utility.EntropyImportance import EntropyImportance
 from utility.CoverageDiversityImportance import CoverageDiversityImportance
-from torch.utils.data import Subset
 from torch.utils.data import DataLoader
 import torch
 import pandas as pd
@@ -23,9 +22,11 @@ test_data = torch.load(f"experiments/{DATASET_NAME}/splits/{DATASET_NAME}_test.p
 
 entropy_calculator = EntropyImportance()
 coverage_calculator = CoverageDiversityImportance()
+frequency_calculator = FrequencyImportance()
 
 entropy_calculator.prepare(train_data + test_data)
 coverage_calculator.prepare(train_data + test_data)
+frequency_calculator.prepare(train_data + test_data)
 
 
 train_dloader = DataLoader(train_data, batch_size=len(train_data), collate_fn=custom_collate_fn, num_workers=24)
@@ -33,6 +34,7 @@ test_dloader = DataLoader(test_data, batch_size=len(test_data), collate_fn=custo
 
 all_entropy_importances = []
 all_coverage_importances = []
+all_frequency_importances = []
 all_users = []
 
 for batch in train_dloader:
@@ -43,11 +45,15 @@ for batch in train_dloader:
     batch_importance = coverage_calculator.calculate_importance(batch)
     all_coverage_importances.extend(batch_importance)
     
+    batch_importance = frequency_calculator.calculate_importance(batch)
+    all_frequency_importances.extend(batch_importance)
+    
     all_users.extend(batch[1].tolist())
     
 data = pd.DataFrame({
     'entropy': all_entropy_importances,
     'coverage': all_coverage_importances,
+    'frequency': all_frequency_importances,
     'user': all_users
     })
 
@@ -60,6 +66,7 @@ import matplotlib.pyplot as plt
 plt.figure(figsize=(8, 6))
 sns.kdeplot(data['entropy'], fill=True, label='Entropy Importance')
 sns.kdeplot(data['coverage'], fill=True, label='Coverage Diversity Importance')
+sns.kdeplot(data['frequency'], fill=True, label='Frequency of Visit Importance')
 
 plt.title(f'Density Plot of importance scores')
 plt.xlabel('Importance Score')
@@ -67,7 +74,7 @@ plt.ylabel('Density')
 plt.legend()
 
 plt.savefig(f"analysis/{DATASET_NAME}/importance_analysis/importances.pdf", bbox_inches='tight', format='pdf')
-
+print(f"Saved importance plot to analysis/{DATASET_NAME}/importance_analysis/importances.pdf")
 
 
 # Plotting density plots for each user (Randomly sample 10 users to visualize)
