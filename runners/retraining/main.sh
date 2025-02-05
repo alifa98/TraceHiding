9ceef0c7456264267bc909a2270e3e461b8ceda7
Ali Faraji <billa.svpc@gmail.com>
2025-02-05 09:50:52 -0500

    feat: add scripts for GRU and LSTM model runner for tracehiding

--- runners/retraining/main.sh
+++ runners/retraining/main.sh
@@ -7,45 +7,52 @@ export LD_LIBRARY_PATH=/usr/local/cuda-12/targets/x86_64-linux/lib/:$LD_LIBRARY_
 source ~/miniconda3/etc/profile.d/conda.sh
 conda activate unlearnF
 
-export CUDA_VISIBLE_DEVICES=5
+export CUDA_VISIBLE_DEVICES=0
 python experiment_scripts/04_1_model_retrainer.py --model GRU --dataset HO_Geolife_Res8 --scenario user --biased entropy_max --sampleSize 1 &
 python experiment_scripts/04_1_model_retrainer.py --model GRU --dataset HO_Geolife_Res8 --scenario user --biased entropy_max --sampleSize 2 &
+export CUDA_VISIBLE_DEVICES=2
 python experiment_scripts/04_1_model_retrainer.py --model GRU --dataset HO_Geolife_Res8 --scenario user --biased entropy_max --sampleSize 3 &
 python experiment_scripts/04_1_model_retrainer.py --model GRU --dataset HO_Geolife_Res8 --scenario user --biased entropy_max --sampleSize 5 &
+export CUDA_VISIBLE_DEVICES=4
 python experiment_scripts/04_1_model_retrainer.py --model LSTM --dataset HO_Geolife_Res8 --scenario user --biased entropy_max --sampleSize 1 &
 python experiment_scripts/04_1_model_retrainer.py --model LSTM --dataset HO_Geolife_Res8 --scenario user --biased entropy_max --sampleSize 2 &
+export CUDA_VISIBLE_DEVICES=1
 python experiment_scripts/04_1_model_retrainer.py --model LSTM --dataset HO_Geolife_Res8 --scenario user --biased entropy_max --sampleSize 3 &
 python experiment_scripts/04_1_model_retrainer.py --model LSTM --dataset HO_Geolife_Res8 --scenario user --biased entropy_max --sampleSize 5 &
 
 
-python experiment_scripts/04_1_model_retrainer.py --model GRU --dataset HO_Porto_Res8 --scenario user --biased entropy_max --sampleSize 4 &
-python experiment_scripts/04_1_model_retrainer.py --model GRU --dataset HO_Porto_Res8 --scenario user --biased entropy_max --sampleSize 21 &
-python experiment_scripts/04_1_model_retrainer.py --model GRU --dataset HO_Porto_Res8 --scenario user --biased entropy_max --sampleSize 43 &
-python experiment_scripts/04_1_model_retrainer.py --model GRU --dataset HO_Porto_Res8 --scenario user --biased entropy_max --sampleSize 88 &
-python experiment_scripts/04_1_model_retrainer.py --model LSTM --dataset HO_Porto_Res8 --scenario user --biased entropy_max --sampleSize 4 &
-python experiment_scripts/04_1_model_retrainer.py --model LSTM --dataset HO_Porto_Res8 --scenario user --biased entropy_max --sampleSize 21 &
-python experiment_scripts/04_1_model_retrainer.py --model LSTM --dataset HO_Porto_Res8 --scenario user --biased entropy_max --sampleSize 43 &
-python experiment_scripts/04_1_model_retrainer.py --model LSTM --dataset HO_Porto_Res8 --scenario user --biased entropy_max --sampleSize 88 &
-
-
+export CUDA_VISIBLE_DEVICES=5
 python experiment_scripts/04_1_model_retrainer.py --model GRU --dataset HO_NYC_Res9 --scenario user --biased entropy_max --sampleSize 2 &
 python experiment_scripts/04_1_model_retrainer.py --model GRU --dataset HO_NYC_Res9 --scenario user --biased entropy_max --sampleSize 11 &
 python experiment_scripts/04_1_model_retrainer.py --model GRU --dataset HO_NYC_Res9 --scenario user --biased entropy_max --sampleSize 23 &
 python experiment_scripts/04_1_model_retrainer.py --model GRU --dataset HO_NYC_Res9 --scenario user --biased entropy_max --sampleSize 46 &
 python experiment_scripts/04_1_model_retrainer.py --model LSTM --dataset HO_NYC_Res9 --scenario user --biased entropy_max --sampleSize 2 &
 python experiment_scripts/04_1_model_retrainer.py --model LSTM --dataset HO_NYC_Res9 --scenario user --biased entropy_max --sampleSize 11 &
+export CUDA_VISIBLE_DEVICES=6
 python experiment_scripts/04_1_model_retrainer.py --model LSTM --dataset HO_NYC_Res9 --scenario user --biased entropy_max --sampleSize 23 &
 python experiment_scripts/04_1_model_retrainer.py --model LSTM --dataset HO_NYC_Res9 --scenario user --biased entropy_max --sampleSize 46 &
 
-
+export CUDA_VISIBLE_DEVICES=7
 python experiment_scripts/04_1_model_retrainer.py --model GRU --dataset HO_Rome_Res8 --scenario user --biased entropy_max --sampleSize 2 &
-python experiment_scripts/04_1_model_retrainer.py --model GRU --dataset HO_Rome_Res8 --scenario user --biased entropy_max --sampleSize 10 &
-python experiment_scripts/04_1_model_retrainer.py --model GRU --dataset HO_Rome_Res8 --scenario user --biased entropy_max --sampleSize 21 &
-python experiment_scripts/04_1_model_retrainer.py --model GRU --dataset HO_Rome_Res8 --scenario user --biased entropy_max --sampleSize 43 &
+python experiment_scripts/04_1_model_retrainer.py --model GRU --dataset HO_Rome_Res8 --scenario user --biased entropy_max --sampleSize 10 
+python experiment_scripts/04_1_model_retrainer.py --model GRU --dataset HO_Rome_Res8 --scenario user --biased entropy_max --sampleSize 21
+python experiment_scripts/04_1_model_retrainer.py --model GRU --dataset HO_Rome_Res8 --scenario user --biased entropy_max --sampleSize 43 
+export CUDA_VISIBLE_DEVICES=0
 python experiment_scripts/04_1_model_retrainer.py --model LSTM --dataset HO_Rome_Res8 --scenario user --biased entropy_max --sampleSize 2 &
 python experiment_scripts/04_1_model_retrainer.py --model LSTM --dataset HO_Rome_Res8 --scenario user --biased entropy_max --sampleSize 10 &
+export CUDA_VISIBLE_DEVICES=2
 python experiment_scripts/04_1_model_retrainer.py --model LSTM --dataset HO_Rome_Res8 --scenario user --biased entropy_max --sampleSize 21 &
 python experiment_scripts/04_1_model_retrainer.py --model LSTM --dataset HO_Rome_Res8 --scenario user --biased entropy_max --sampleSize 43 &
 
+export CUDA_VISIBLE_DEVICES=5
+python experiment_scripts/04_1_model_retrainer.py --model GRU --dataset HO_Porto_Res8 --scenario user --biased entropy_max --sampleSize 4 &
+python experiment_scripts/04_1_model_retrainer.py --model GRU --dataset HO_Porto_Res8 --scenario user --biased entropy_max --sampleSize 21 &
+python experiment_scripts/04_1_model_retrainer.py --model GRU --dataset HO_Porto_Res8 --scenario user --biased entropy_max --sampleSize 43 &
+python experiment_scripts/04_1_model_retrainer.py --model GRU --dataset HO_Porto_Res8 --scenario user --biased entropy_max --sampleSize 88 
+export CUDA_VISIBLE_DEVICES=6
+python experiment_scripts/04_1_model_retrainer.py --model LSTM --dataset HO_Porto_Res8 --scenario user --biased entropy_max --sampleSize 4
+python experiment_scripts/04_1_model_retrainer.py --model LSTM --dataset HO_Porto_Res8 --scenario user --biased entropy_max --sampleSize 21 &
+python experiment_scripts/04_1_model_retrainer.py --model LSTM --dataset HO_Porto_Res8 --scenario user --biased entropy_max --sampleSize 43
+python experiment_scripts/04_1_model_retrainer.py --model LSTM --dataset HO_Porto_Res8 --scenario user --biased entropy_max --sampleSize 88
 
 cd "$ORIGINAL_DIR" || { echo "Failed to return to the original directory"; exit 1; }
---
+++ runners/tracehiding/gru_rome.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+ORIGINAL_DIR=$(pwd)
+cd /local/data1/users/faraji/unlearning_experiments_new || { echo "Failed to change directory"; exit 1; }
+
+export LD_LIBRARY_PATH=/usr/local/cuda-12/targets/x86_64-linux/lib/:$LD_LIBRARY_PATH
+
+source ~/miniconda3/etc/profile.d/conda.sh
+conda activate unlearnF
+
+export CUDA_VISIBLE_DEVICES=0
+
+python experiment_scripts/05_1_our_method.py --model GRU --dataset HO_Rome_Res8 --scenario user --biased entropy_max --sampleSize 2 --batchSize 20  --importance entropy & 
+python experiment_scripts/05_1_our_method.py --model GRU --dataset HO_Rome_Res8 --scenario user --biased entropy_max --sampleSize 10 --batchSize 20  --importance entropy & 
+python experiment_scripts/05_1_our_method.py --model GRU --dataset HO_Rome_Res8 --scenario user --biased entropy_max --sampleSize 21 --batchSize 20  --importance entropy & 
+python experiment_scripts/05_1_our_method.py --model GRU --dataset HO_Rome_Res8 --scenario user --biased entropy_max --sampleSize 43 --batchSize 20  --importance entropy &
+python experiment_scripts/05_1_our_method.py --model GRU --dataset HO_Rome_Res8 --scenario user --biased entropy_max --sampleSize 2 --batchSize 20 --importance coverage_diversity & 
+python experiment_scripts/05_1_our_method.py --model GRU --dataset HO_Rome_Res8 --scenario user --biased entropy_max --sampleSize 10 --batchSize 20  --importance coverage_diversity & 
+python experiment_scripts/05_1_our_method.py --model GRU --dataset HO_Rome_Res8 --scenario user --biased entropy_max --sampleSize 21 --batchSize 20  --importance coverage_diversity & 
+python experiment_scripts/05_1_our_method.py --model GRU --dataset HO_Rome_Res8 --scenario user --biased entropy_max --sampleSize 43 --batchSize 20  --importance coverage_diversity &
+python experiment_scripts/05_1_our_method.py --model GRU --dataset HO_Rome_Res8 --scenario user --biased entropy_max --sampleSize 2 --batchSize 20  --importance uuniqe &
+python experiment_scripts/05_1_our_method.py --model GRU --dataset HO_Rome_Res8 --scenario user --biased entropy_max --sampleSize 10 --batchSize 20  --importance uuniqe &
+python experiment_scripts/05_1_our_method.py --model GRU --dataset HO_Rome_Res8 --scenario user --biased entropy_max --sampleSize 21 --batchSize 20  --importance uuniqe &
+python experiment_scripts/05_1_our_method.py --model GRU --dataset HO_Rome_Res8 --scenario user --biased entropy_max --sampleSize 43 --batchSize 20  --importance  uuniqe &
+
+
+cd "$ORIGINAL_DIR" || { echo "Failed to return to the original directory"; exit 1; }
