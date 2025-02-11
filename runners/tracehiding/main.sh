ac6537d94cf2fcfd50177cf247afca0737e22c4f
Ali Faraji <ali@faraji.info>
2025-02-11 16:02:48 -0500

    fix: correct spelling of 'porto' in baseline and tracehiding runner scripts

--- runners/tracehiding/main.sh
+++ runners/tracehiding/main.sh
@@ -3,9 +3,9 @@
 bash lstm_nyc.sh
 bash lstm_geolife.sh
 bash lstm_rome.sh
-bash lstm_porot.sh
+bash lstm_porto.sh
 
 bash gru_nyc.sh
 bash gru_geolife.sh
 bash gru_rome.sh
-bash gru_porot.sh
+bash gru_porto.sh
