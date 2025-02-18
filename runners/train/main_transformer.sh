e6f068edba0bf8c1fe0523ee84d32813ea9313f9
Ali Faraji <ali@faraji.info>
2025-02-18 01:12:58 -0500

    feat: disable Triton optimization for compatibility with older GPUs

--- runners/train/main_transformer.sh
+++ runners/train/main_transformer.sh
@@ -7,6 +7,8 @@ export LD_LIBRARY_PATH=/usr/local/cuda-12/targets/x86_64-linux/lib/:$LD_LIBRARY_
 source ~/miniconda3/etc/profile.d/conda.sh
 conda activate unlearnF
 
+export TORCH_COMPILE_DISABLE=0 # to disable the Triton optimization (the old gpus are not compatible with it)
+
 export CUDA_VISIBLE_DEVICES=0
 python experiment_scripts/02_5_modernBERT_trainer.py ModernBERT HO_Rome_Res8 &
 export CUDA_VISIBLE_DEVICES=1
