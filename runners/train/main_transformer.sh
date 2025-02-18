6ce7b90848465f743b594e852b8ec584067ce3de
Ali Faraji <ali@faraji.info>
2025-02-18 01:28:38 -0500

    feat: update training script to use BERT model for various datasets

--- runners/train/main_transformer.sh
+++ runners/train/main_transformer.sh
@@ -9,13 +9,22 @@ conda activate unlearnF
 
 export TORCH_COMPILE_DISABLE=0 # to disable the Triton optimization (the old gpus are not compatible with it)
 
+# export CUDA_VISIBLE_DEVICES=0
+# python experiment_scripts/02_5_transformer_trainer.py ModernBERT HO_Rome_Res8 &
+# export CUDA_VISIBLE_DEVICES=1
+# python experiment_scripts/02_5_transformer_trainer.py ModernBERT HO_Porto_Res8 &
+# export CUDA_VISIBLE_DEVICES=2
+# python experiment_scripts/02_5_transformer_trainer.py ModernBERT HO_Geolife_Res8 &
+# export CUDA_VISIBLE_DEVICES=6
+# python experiment_scripts/02_5_transformer_trainer.py ModernBERT HO_NYC_Res9 &
+
 export CUDA_VISIBLE_DEVICES=0
-python experiment_scripts/02_5_modernBERT_trainer.py ModernBERT HO_Rome_Res8 &
-export CUDA_VISIBLE_DEVICES=1
-python experiment_scripts/02_5_modernBERT_trainer.py ModernBERT HO_Porto_Res8 &
+python experiment_scripts/02_5_transformer_trainer.py BERT HO_Rome_Res8 &
 export CUDA_VISIBLE_DEVICES=2
-python experiment_scripts/02_5_modernBERT_trainer.py ModernBERT HO_Geolife_Res8 &
+python experiment_scripts/02_5_transformer_trainer.py BERT HO_Porto_Res8 &
+export CUDA_VISIBLE_DEVICES=4
+python experiment_scripts/02_5_transformer_trainer.py BERT HO_Geolife_Res8 &
 export CUDA_VISIBLE_DEVICES=6
-python experiment_scripts/02_5_modernBERT_trainer.py ModernBERT HO_NYC_Res9 &
+python experiment_scripts/02_5_transformer_trainer.py BERT HO_NYC_Res9 &
 
 cd "$ORIGINAL_DIR" || { echo "Failed to return to the original directory"; exit 1; }
