74c21c2f534943ba7f9c52b8154b6aba72028363
Ali Faraji <ali@faraji.info>
2025-04-21 22:03:41 -0400

    fix: fix the scipt path for training rnns

--- runners/train/rnn.sh
+++ runners/train/rnn.sh
@@ -8,23 +8,23 @@ source ~/miniconda3/etc/profile.d/conda.sh
 conda activate unlearnF
 
 export CUDA_VISIBLE_DEVICES=0
-python experiment_scripts/02_1_model_trainer.py LSTM HO_Rome_Res8 &
+python experiment_scripts/02_1_rnn_trainer.py LSTM HO_Rome_Res8 &
 export CUDA_VISIBLE_DEVICES=1
-python experiment_scripts/02_1_model_trainer.py GRU  HO_Rome_Res8 &
+python experiment_scripts/02_1_rnn_trainer.py GRU  HO_Rome_Res8 &
 
 export CUDA_VISIBLE_DEVICES=2
-python experiment_scripts/02_1_model_trainer.py LSTM HO_Geolife_Res8 &
+python experiment_scripts/02_1_rnn_trainer.py LSTM HO_Geolife_Res8 &
 export CUDA_VISIBLE_DEVICES=4
-python experiment_scripts/02_1_model_trainer.py GRU  HO_Geolife_Res8 & 
+python experiment_scripts/02_1_rnn_trainer.py GRU  HO_Geolife_Res8 & 
 
 export CUDA_VISIBLE_DEVICES=6
-python experiment_scripts/02_1_model_trainer.py LSTM HO_NYC_Res9 & 
+python experiment_scripts/02_1_rnn_trainer.py LSTM HO_NYC_Res9 & 
 export CUDA_VISIBLE_DEVICES=7
-python experiment_scripts/02_1_model_trainer.py GRU  HO_NYC_Res9 &
+python experiment_scripts/02_1_rnn_trainer.py GRU  HO_NYC_Res9 &
 
 export CUDA_VISIBLE_DEVICES=5
-python experiment_scripts/02_1_model_trainer.py LSTM HO_Porto_Res8 &
-python experiment_scripts/02_1_model_trainer.py GRU  HO_Porto_Res8 &
+python experiment_scripts/02_1_rnn_trainer.py LSTM HO_Porto_Res8 &
+python experiment_scripts/02_1_rnn_trainer.py GRU  HO_Porto_Res8 &
 
 
 cd "$ORIGINAL_DIR" || { echo "Failed to return to the original directory"; exit 1; }
